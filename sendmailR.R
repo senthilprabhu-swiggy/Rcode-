b0cf64262151f9a28ef0ea40d618f7b739dc8ea8
senthilprabhu-swiggy <54438032+senthilprabhu-swiggy@users.noreply.github.com>
2020-04-15 15:21:36 +0530

    Delete sendmailR.R

--- sendmailR.R
+++
@@ -1,9 +0,0 @@
-install.packages("mailR", dep = T)
-library(mailR)
-send.mail (from = "pallavi.venkateshan@swiggy.in",
-           to = "pvvortex@gmail.com",
-           subject = "Subject of the email",
-           body = "Body of the email",
-           smtp = list(host.name = "smtp.gmail.com", port = 465, user.name = "pallavi.venkateshan@swiggy.in", passwd = "Skadoosh102##$", ssl = TRUE),
-           authenticate = TRUE,
-           send = TRUE)
